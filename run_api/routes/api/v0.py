a9cb217d3e4f43ab10e56199a6d81aa150533454
Eugene <gish.ee18@gmail.com>
2019-11-11 19:45:00 +0300

    Fix language info endpoints

--- run_api/routes/api/v0.py
+++ run_api/routes/api/v0.py
@@ -11,6 +11,18 @@ from ...constants import SUPPORTED_LANGUAGES
 routes = web.RouteTableDef()
 
 
+@routes.get(r"/languages/{language_code}")
+async def get_language(req: web.Request) -> web.Response:
+    language = validate_language(req.match_info["language_code"])
+
+    return web.json_response(language.to_json())
+
+
+@routes.get("/languages")
+async def get_languages(req: web.Request) -> web.Response:
+    return web.json_response([l.to_json() for l in SUPPORTED_LANGUAGES.values()])
+
+
 @routes.post(r"/languages/{language_code}")
 async def run_code(req: web.Request) -> web.Response:
     validate_language(req.match_info["language_code"])
@@ -46,11 +58,6 @@ async def kill_session(req: web.Request) -> web.Response:
     # TODO
 
     return web.json_response(dict(message=f"Killed session {session_id}"))
-
-
-@routes.get(r"/sessions/{session_id:(\w|\d)+}/kill")
-async def get_languages(req: web.Request) -> web.Response:
-    return web.json_response([l.to_json() for l in SUPPORTED_LANGUAGES.values()])
 
 
 @routes.get("/endpoints")
