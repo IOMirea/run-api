3dde7fa02ab7e2d5d2f67202f9418aed272fc690
Eugene <gish.ee18@gmail.com>
2019-11-16 23:06:01 +0300

    Add compiled languages support

--- run_api/routes/api/v0.py
+++ run_api/routes/api/v0.py
@@ -39,14 +39,13 @@ async def run_code(req: web.Request) -> web.Response:
     except JSONDecodeError:
         raise web.HTTPBadRequest(reason="Bad json in body")
 
-    code = data.pop("code", None)
-    if code is None or code == "":
-        raise web.HTTPBadRequest(reason="Missing or empty code value")
-
-    payload = {"code": code}
+    payload = {"code": data.pop("code", language.example)}
 
     if language.compiled:
-        payload["compile_args"] = data.pop("compile_args", language.compile_args)
+        compile_args = data.pop("compile_args", language.compile_args)
+        payload["compile_command"] = f"{language.compiler} {compile_args}"
+
+    payload["merge_output"] = data.get("merge_output", False)
 
     backend_run_url = (
         f"{req.config_dict['config']['app']['run-lb-ip']}/run/{language.name}"
--- run_api/types/language.py
+++ run_api/types/language.py
@@ -5,7 +5,7 @@ from typing import Any, List, Optional
 
 class Language:
 
-    __slots__ = ("name", "aliases", "active", "example", "compile_args")
+    __slots__ = ("name", "aliases", "active", "example", "compiler", "compile_args")
 
     def __init__(
         self,
@@ -14,12 +14,14 @@ class Language:
         aliases: List[str],
         active: bool,
         example: str,
+        compiler: str,
         compile_args: Optional[str] = None,
     ):
         self.name = name
         self.aliases = aliases
         self.active = active
         self.example = example
+        self.compiler = compiler
         self.compile_args = compile_args
 
     @classmethod
@@ -29,22 +31,20 @@ class Language:
             aliases=data["aliases"],
             active=data.get("active", True),
             example=data["example"],
+            compiler=data.get("compiler"),
             compile_args=data.get("compile_args"),
         )
 
     def to_json(self) -> Any:
         result = {}
-        for property in ("name", "aliases", "example", "compiled"):
+        for property in ("name", "aliases", "example", "compiled", "compile_args"):
             result[property] = getattr(self, property)
-
-        if self.compiled:
-            result["compile_args"] = self.compile_args
 
         return result
 
     @property
     def compiled(self) -> bool:
-        return self.compile_args is not None
+        return self.compiler is not None and self.compile_args is not None
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} name={self.name} active={self.active}>"
